1551ae78601964b4eee6990d16fe682158d2d4ca
Philou <pfouque@gmail.com>
2020-05-01 22:59:03 +0200

    Typos and new color for add person button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title: string = 'Angular Bootstrap Sandbox';
+  public title: string = 'Angular Bootstrap Showroom';
 }
