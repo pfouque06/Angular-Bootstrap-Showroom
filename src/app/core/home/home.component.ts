bd8cbf4c224be31693b60cd5cff33f92f7c64ab2
Philou <pfouque@gmail.com>
2020-05-16 23:40:51 +0200

    move Home/accordion to home/collapsed sections

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public isFirstCollapsed = true;
+  public isSecondCollapsed = true;
+  public isThirdCollapsed = true;
+
   constructor() { }
 
   ngOnInit(): void {
