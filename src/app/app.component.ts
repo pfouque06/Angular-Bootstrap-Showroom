ce85a050eba7ef61cb5ebafa7046731efc4e0b71
Philou <pfouque@gmail.com>
2020-05-24 18:50:00 +0200

    add NavigateAndScrollInService Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { NgAnimateScrollService } from 'ng-animate-scroll';
 import { AnimateService } from 'src/app/shared/service/animate.service';
+import { NavigateAndScrollInService } from './shared/service/navigate-and-scroll-in.service';
 
 @Component({
   selector: 'app-root',
@@ -12,54 +13,19 @@ import { AnimateService } from 'src/app/shared/service/animate.service';
     AnimateService.TslideDownGate,
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title: string = 'Angular Bootstrap Showroom';
 
 
   public isScrollUpVisible: boolean;
 
-  // Declare height and width variables
-  displayHeight:any;
-  displayWidth:any;
+  @ViewChild('base', {read: ElementRef}) baseTag: ElementRef;
 
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?) {
-    this.displayHeight = window.innerHeight;
-    this.displayWidth = window.innerWidth;
-    // console.log("app.getScreenSize(): ["+this.displayHeight, this.displayWidth +"]");
-    // process ScrollUp button visibility
-    this.toggleScrollUp();
-  }
+  constructor(public scrollService: NavigateAndScrollInService) { }
 
-  // toptag relative position
-  toptagTop:any;
-  @ViewChild('toptag', {read: ElementRef}) toptag: ElementRef;
-  @HostListener('window:scroll', ['$event'])
-  getTopPositionOnScroll(event?) {
-    if ( this.toptag != null) {
-      this.toptagTop = this.toptag.nativeElement.getBoundingClientRect().top;
-      // console.log("getTopPositionOnScroll(): " + this.toptagTop);
-      // process ScrollUp button visibility
-      this.toggleScrollUp();
-    }
-  }
+  ngOnInit(): void { }
 
-  toggleScrollUp() {
-    if (this.displayHeight < 400)  { this.isScrollUpVisible = false;}
-    else { // display bug enough to handle scrollup nav button
-      if (this.toptagTop < -200) { this.isScrollUpVisible = true;}
-      else { this.isScrollUpVisible = false;}
-    }
-    // console.debug("toggleScrollUp("+this.isScrollUpVisible+")");
-  }
-
-  constructor(private animateScrollService: NgAnimateScrollService) {
-    // get dynamic screen size and process to ScrollUp button visibility logic
-    this.getScreenSize();
-    this.getTopPositionOnScroll();
-  }
-
-  public scrollToElement(element: any, duration?:number) {
-    this.animateScrollService.scrollToElement(element, duration ? duration : 300);
+  ngAfterViewInit() {
+    this.scrollService.setbaseTag('base', this.baseTag);
   }
 }
